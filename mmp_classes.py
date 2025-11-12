bb292f3a79160615e0b9e106051c4d7373a57303
staunton-golding <hankghgold@gmail.com>
2025-11-12 15:47:41 -0700

    refactored panoramic rendering, cleaned up / commented other files
    added dem_getter (from usgs). Refactored panoramic rendering (with interpolation to account for poor angular resolution with mountains close to the location of the image)

--- mmp_classes.py
+++ mmp_classes.py
@@ -1,10 +1,11 @@
 import time
 import numpy as np
 import pandas as pd
-
+import cv2
 from PIL import Image
 from pillow_heif import register_heif_opener
 from numba import njit
+from scipy.interpolate import make_smoothing_spline, CubicSpline, UnivariateSpline
 
 #for opening iphone .heic photos
 register_heif_opener()
@@ -142,11 +143,10 @@ class MidpointIdx(object):
 
 #fill array containing panoramic image (start with values closest to photo location, work your way out, as to not include peaks / elevations obscured by bigger mountains
 @njit
-def _fill_im_array(imshow_arr, r_sorted, im_elev, theta_round, color, peak_check_mask, elev_add):
-    
-    for idx in range(len(r_sorted)):
+def fill_im_array_njit(imshow_arr, r_sorted, im_elev, theta_round, color, peak_check_mask, elev_add, idx_comp):
+
+    for idx in range(idx_comp, len(r_sorted)):
         theta = theta_round[r_sorted[idx]]
-        rad = r_sorted[idx]
         elev = im_elev[r_sorted[idx]]
         
         if imshow_arr[elev, theta] == 0:
@@ -155,7 +155,87 @@ def _fill_im_array(imshow_arr, r_sorted, im_elev, theta_round, color, peak_check
             
         if imshow_arr[elev+elev_add, theta] != 0: #sometimes true peaks are slightly obscured, while the mountain is wholly visible. elev_add solves for this
             peak_check_mask[r_sorted[idx]] = 0
+         
+    return imshow_arr, peak_check_mask
+
+def _fill_im_array(imshow_arr, r_sorted, rad, im_elev, theta_round, color, peak_check_mask, elev_add, lam_input, rad_input):
+
+    rad_div = rad_input #distance of radial ring (value X30m)
+    n_div = 1 #radial ring counter
+    idx = 0 #idx counter
+    
+    length_imshow = imshow_arr.shape[1]
+
+    num_end = 250 #beyond ~7500 meters, no need to interpolate, good resolution
+    #start with radial ring interpolations (every rad_input, take all values in that ring, interpolate)
+    while n_div < num_end:
+        temp_arr_theta = []
+        temp_arr_elev = []
+        idx_temp_mask_check = []
+        while rad[r_sorted[idx]] < n_div * rad_div:
+            temp_arr_theta.append(theta_round[r_sorted[idx]])
+            temp_arr_elev.append(im_elev[r_sorted[idx]])
+            idx_temp_mask_check.append(idx)
+            idx += 1
+
+        #now we have the im_elev and theta_round pairs in a given radial ring
+        max_theta_idx = np.argmax(temp_arr_theta)
+        min_theta_idx = np.argmin(temp_arr_theta)
+
+        #for wrap around interpolation (function is grid size agnostic) (append wraparound values)
+        temp_arr_theta.append(temp_arr_theta[max_theta_idx] - length_imshow - 1)
+        temp_arr_elev.append(temp_arr_elev[max_theta_idx])
+
+        temp_arr_theta.append(temp_arr_theta[min_theta_idx] + length_imshow)
+        temp_arr_elev.append(temp_arr_elev[min_theta_idx])
+
+        temp_arr_elev = np.asarray(temp_arr_elev)
+        temp_arr_theta = np.asarray(temp_arr_theta)
+        
+        temp_arg_sort = np.argsort(temp_arr_theta) #for interpolation
+        
+        arr_theta = temp_arr_theta[temp_arg_sort]
+        arr_elev = temp_arr_elev[temp_arg_sort]
+        
+        arr_elev_unique = []
+        unique_theta, unique_idx_theta, unique_count_theta = np.unique(arr_theta, return_counts = True, return_index = True)
+        
+        for idx_unique in range(len(unique_idx_theta)):
+            if unique_count_theta[idx_unique] != 1:
+                arr_elev_unique.append(np.max(arr_elev[unique_idx_theta[idx_unique]:unique_idx_theta[idx_unique]+unique_count_theta[idx_unique]-1])) #max elev of same thetas
+            else:
+                arr_elev_unique.append(arr_elev[unique_idx_theta[idx_unique]]) #elev of a given unique theta
+
+        arr_theta = arr_theta[unique_idx_theta]
+        arr_elev_unique = np.asarray(arr_elev_unique)
+
+        temp_spline = make_smoothing_spline(arr_theta, arr_elev_unique, lam = lam_input) #lam is smoothing factor, make smoothing spline
+        thetas_fill = np.round(np.arange(0,length_imshow)).astype(int)
+    
+        elevs_fill = np.round(temp_spline.__call__(thetas_fill)).astype(int) #evaluate smoothing spline (elevations) at all thetas
+
+        #go through radial ring and check for obscured peaks
+        for idx_mask in idx_temp_mask_check:
+            elev = im_elev[r_sorted[idx_mask]]
+            theta = theta_round[r_sorted[idx_mask]] - 1
+            if imshow_arr[elev+elev_add, theta] != 0: #sometimes true peaks are slightly obscured, while the mountain is wholly visible. elev_add solves for this
+                peak_check_mask[r_sorted[idx_mask]] = 0 
+
+        #fill in im with interpolated radial ring
+        for theta_idx in range(len(thetas_fill)):  
+            theta = thetas_fill[theta_idx]
+            elev = elevs_fill[theta_idx]
             
+            if imshow_arr[elev, theta] == 0:
+                first_nonzero_row = np.nonzero(imshow_arr[:, theta] == 0)[0][0]
+                imshow_arr[first_nonzero_row:elev, theta] = color[r_sorted[idx]]
+
+        n_div += 1
+
+    #now fill out the rest all normal style
+    idx_comp = idx
+    imshow_arr, peak_check_mask = fill_im_array_njit(imshow_arr, r_sorted, im_elev, theta_round, color, peak_check_mask, elev_add, idx_comp)
+
     return imshow_arr, peak_check_mask
 
 #calculate distance between two coordinates
@@ -188,7 +268,7 @@ def _color_fill(rad, max_r):
 
 class PolarImage(object):
 
-    def __init__(self, band1, gt, midpoint, lat, aoi_P1, row_indices, elev_add, df_selected, elev_photo):
+    def __init__(self, band1, gt, midpoint, lat, aoi_P1, row_indices, elev_add, df_selected, elev_photo, lam_input, rad_in):
         
         self.midpoint = midpoint
         self.lat = lat
@@ -200,24 +280,31 @@ class PolarImage(object):
         self.elev_add = elev_add
 
         #convert DEM to adjusted polar coordinates
+        start = time.time()
         self.cart2pol_im(self.band1, self.midpoint, self.delta_deg, self.lat, elev_photo)
-        imshow_arr = np.zeros((np.max(self.elev_phi)+50,np.max(self.theta_round)))
+        end = time.time()
+        print(f"Done with cart2pol in {int(np.round(end - start))} seconds")
+
+        imshow_arr = np.zeros((np.max(self.elev_phi)+250,np.max(self.theta_round))) #instantiate image array
+        self.imshow_arr = imshow_arr
+
         #Convert mountain peaks to adjusted polar coordinates
         self.unwrap_peak_labels(self.aoi_P1, self.row_indices, self.midpoint, self.band1, self.elev_phi, df_selected)
+
         #fill panoramic image (DEM data and peaks)
         start = time.time()
-        self.pano_image_arr, self.peak_check_mask = self.fill_im_array(imshow_arr, self.r_sorted, self.elev_phi, self.theta_round, self.color, self.peak_check_mask, self.elev_add)
+        self.pano_image_arr, self.peak_check_mask = self.fill_im_array(imshow_arr, self.r_sorted, self.rad, self.elev_phi, self.theta_round, self.color, self.peak_check_mask, self.elev_add, lam_input, rad_in)
         end = time.time()
         print(f"Done with filling pano image aray in {int(np.round(end - start))} seconds")
-        #correct for zero column artifacts
-        self.artifact_correction_pano_image()
-        self.artifact_correction_peaks()
-    
+
+        #update peaks to only include visible peaks
+        self.update_peaks()
+
     def haversine(self, cols_corrected, delta_deg, rows_corrected, lat_orig):
         return _haversine(cols_corrected, delta_deg, rows_corrected, lat_orig)       
         
-    def fill_im_array(self, imshow_arr, r_sorted, im_elev, theta_round, color, peak_check_mask, elev_add):
-        return _fill_im_array(imshow_arr, r_sorted, im_elev, theta_round, color, peak_check_mask, elev_add)      
+    def fill_im_array(self, imshow_arr, r_sorted, rad, im_elev, theta_round, color, peak_check_mask, elev_add, lam_input, rad_in):
+        return _fill_im_array(imshow_arr, r_sorted, rad, im_elev, theta_round, color, peak_check_mask, elev_add, lam_input, rad_in)      
    
     def arct(self, a):
         return _arct(a)
@@ -232,7 +319,6 @@ class PolarImage(object):
         return _color_fill(rad, max_r)
     
     def cart2pol_im(self, elev_data, midpoint, delta_deg, lat_orig, elev_photo):
-        start = time.time()
         #cols and rows as indices
         cols = np.arange(0, elev_data.shape[0])
         cols = np.hstack((cols,)*elev_data.shape[1])
@@ -240,14 +326,16 @@ class PolarImage(object):
         rows = np.arange(0, elev_data.shape[1]).reshape(-1,1)
         rows = np.tile(rows, elev_data.shape[0]).flatten()
         
-        #don't need pixel / idx from where image was taken (radius will be 0, theta will be confused)
-        cols_no_mp = np.delete(cols, midpoint[0])
-        rows_no_mp = np.delete(rows, midpoint[1])
-    
         #cols and rows indices centered around image location
-        cols_corrected = cols_no_mp - midpoint[0]
-        rows_corrected = rows_no_mp - midpoint[1]
-        
+        cols_adjusted = cols - midpoint[1] #CHECK HERE ALSO
+        rows_adjusted = rows - midpoint[0]
+
+        unwrapped_mp_idx = np.ravel_multi_index([midpoint[0], midpoint[1]], elev_data.shape)
+        self.un_mp_idx = unwrapped_mp_idx
+        #don't need pixel / idx from where image was taken (radius will be 0, theta will be confused)
+        cols_corrected = np.delete(cols_adjusted, unwrapped_mp_idx)
+        rows_corrected = np.delete(rows_adjusted, unwrapped_mp_idx)
+
         #haversine formula, from https://www.movable-type.co.uk/scripts/latlong.html
         a = self.haversine(cols_corrected, delta_deg, rows_corrected, lat_orig)
         c = self.arct(a)
@@ -256,25 +344,33 @@ class PolarImage(object):
         delt_elev = d**2 / (2*R)
         
         im_arr = elev_data.flatten()
-        im_arr_no_mp = np.delete(im_arr, np.ravel_multi_index([midpoint[0], midpoint[1]], elev_data.shape))
-    
-       #convert x and y into modified polar
-        theta = self.atan_theta(cols_corrected, rows_corrected)
-        theta_round = np.round((theta+np.pi)*4, decimals = 2)*100 #reduce num theta for more efficient image processing, increasing decimals increases horizontal resolution
-        rad, r_sorted = self.sort_rad(rows_corrected, cols_corrected)
+        im_arr_no_mp = np.delete(im_arr, unwrapped_mp_idx)
+        
+        #convert x and y into modified polar
+        theta = self.atan_theta(rows_corrected, cols_corrected)
+        theta_round = np.round((theta+np.pi)*75, decimals = 1)*10 #reduce num theta for more efficient image processing, increasing decimals increases horizontal resolution
+        rad, r_sorted = self.sort_rad(cols_corrected, rows_corrected)
         theta_round = theta_round.astype(int)
-        elev_corrected = (im_arr_no_mp - delt_elev).astype(int)
+
+        elev_photo = elev_photo + 2 #most humans dont take photos 1" off the ground
+        
+        elev_corrected = (im_arr_no_mp - delt_elev - elev_photo).astype(int)
+      
+        self.delt_elev = delt_elev
+        self.elev_photo = elev_photo
+        self.elev_corrected = elev_corrected
         
         max_r = np.max(rad)
         color = self.color_fill(rad, max_r)
-        color[elev_corrected<0] = 0 #to allow for elimination of all zero columns in fill_im_array step
+        color[elev_corrected<(-1*elev_photo)] = 0
         
-        elev_corrected[elev_corrected<0] = 0
+        elev_phi = elev_corrected / rad
+
+        elev_phi = elev_phi + np.abs(np.min(elev_phi))
+        elev_phi = 1000 * (elev_phi / np.max(elev_phi)) #normalize 0 - 1000
         
-        elev_phi = np.arctan2((elev_corrected - elev_photo),rad)
-        elev_phi = elev_phi+np.abs(np.min(elev_phi))
-        elev_phi = np.nan_to_num(elev_phi)
-        elev_phi_round = np.round((elev_phi+np.pi)*4, decimals = 2)*100
+        elev_phi_round = np.round(elev_phi)+500
+        
         elev_phi_round = elev_phi_round.astype(int)
         
         self.theta_round = theta_round
@@ -282,21 +378,19 @@ class PolarImage(object):
         self.rad = rad
         self.elev_phi = elev_phi_round
         self.color = color
-        end = time.time()
-        print(f"Done with cart2pol in {int(np.round(end - start))} seconds")
         
     def unwrap_peak_labels(self, aoi_P1, row_indices, midpoint, band1, elev_phi, df_selected):
         #names of all peaks in bounding box
-        start = time.time()
         names_used = df_selected['Feature Name']
         names_used = names_used.iloc[row_indices].reset_index()
         names_used = names_used.iloc[:,1]
-        
-        xs_p1 = aoi_P1[:,1] #xs
-        ys_p1 = aoi_P1[:,0] #ys
+
+        xs_p1 = aoi_P1[:,0] #xs
+        ys_p1 = aoi_P1[:,1] #ys
 
         #idx of peaks in flattened array (masked in this way due to multiple naming conventions - some peaks are within the same 30m x 30m box, so can't just make mask overlayed on DEM, need specific theta and elevation of each peak (encoded in flattened array idx).
-        idx_peaks = np.ravel_multi_index([xs_p1, ys_p1], band1.shape)
+        idx_peaks = np.ravel_multi_index([ys_p1, xs_p1], band1.shape)
+        #idx_peaks[idx_peaks>self.un_mp_idx] = idx_peaks[idx_peaks>self.un_mp_idx]-1
         elevs_aoi = elev_phi[idx_peaks]
         
         peak_check_mask = np.zeros((len(band1.flatten()),1))
@@ -307,28 +401,17 @@ class PolarImage(object):
         ys_p1_theta = ys_p1 - midpoint[0]
         
         theta_peak = np.arctan2(ys_p1_theta, xs_p1_theta) ### Y FIRST, CHECK DOCS AGAIN TM
-        theta_round_peak = (np.round((theta_peak + np.pi)*4, decimals = 2)*100).astype(int)
+        theta_round_peak = (np.round((theta_peak + np.pi)*75, decimals = 1)*10).astype(int)
         
         name_peak_df = pd.DataFrame({'Elevations': elevs_aoi, 'Theta': theta_round_peak, 'idx peaks': idx_peaks, 'Peak Name': names_used}).reset_index()
 
         self.name_peak_df = name_peak_df
         self.peak_check_mask = peak_check_mask
-        end = time.time()
-        print(f"Done with unwrap_peak_labels in {int(np.round(end - start))} seconds")
-
-    def artifact_correction_pano_image(self):
-        #theta equisampled from min to max theta with 1,000 steps per rad. Not all theta have values. Need to eliminate these zero-columns
-        self.art_cor_idx = np.where(np.all(self.pano_image_arr == 0, axis=0))[0]
-        self.art_cor_pano_image = self.pano_image_arr[:,~np.all(self.pano_image_arr == 0, axis=0)]
-        del self.pano_image_arr
-
-    def artifact_correction_peaks(self):
-        start = time.time()
-        #after pano image artifact corrected, need to shift peaks in image to left to account for deleted zero-columns
         
+    def update_peaks(self):
         #idx of all peaks still visible
         idx_of_peaks = np.nonzero(self.peak_check_mask)[0]
-
+        
         #filter df to have only visible peaks
         all_idx_in_img = self.name_peak_df['idx peaks'].isin(idx_of_peaks)
         mountains_in_image = self.name_peak_df[all_idx_in_img == True]
@@ -340,22 +423,12 @@ class PolarImage(object):
         
         #sort theta (theta guaranteed << 10,000, big O not a concern)
         theta_sort_idx = np.argsort(theta_peaks_adjust)
-        
-        #unadjusted, need to hold original values (e.g. theta = 225 with idx to be deleted = 224, 223, 222, 119, 117. Without, this theta would move to 222, but 223 and 224 wouldn't be counted)
-        theta_sorted_temp_arr = theta_peaks_adjust[theta_sort_idx]
 
         #sort all needed variables
         names_sorted = names_peaks[theta_sort_idx]
         mt_index_adjust = mt_index_adjust[theta_sort_idx]
         theta_peaks_adjust_sorted = theta_peaks_adjust[theta_sort_idx]
         elev_peaks_adjust_sorted = elev_peaks_adjust[theta_sort_idx]
-        
-        #find first theta (from orig sorted array) bigger than deleted column, shift all thetas bigger left by one.
-        for jj in range(len(self.art_cor_idx)):
-            first_theta_greater = np.nonzero(theta_sorted_temp_arr[:] >= self.art_cor_idx[jj])[0][0]
-            theta_peaks_adjust_sorted[first_theta_greater:] -= 1
-        end = time.time()
-        print(f"Done with artifact correction in {int(np.round(end - start))} seconds")
-        self.name_peak_df_art_cor = pd.DataFrame({'Elevations': elev_peaks_adjust_sorted, 'Theta': theta_peaks_adjust_sorted, 'Peak Name': names_sorted, 'Mountain Index': mt_index_adjust})
-        del self.name_peak_df, self.peak_check_mask
-        
+
+        #update self.name_peak_df
+        self.name_peak_df = pd.DataFrame({'Elevations': elev_peaks_adjust_sorted, 'Theta': theta_peaks_adjust_sorted, 'Peak Name': names_sorted, 'Mountain Index': mt_index_adjust})
